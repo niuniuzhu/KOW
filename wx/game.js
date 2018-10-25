a9c2d83bd289a418d73c5a90f514a0de1ef9abb1
Ron <niuniuzhu.cn@gmail.com>
2018-10-26 00:37:58 +0800

    ws适配

--- wx/game.js
+++ wx/game.js
@@ -1,8 +1,10 @@
-import './js/Libs/weapp-adapter'
-import './js/Libs/symbol'
+import "./js/Libs/weapp-adapter.js";
 import "./js/Libs/laya.core.js";
+import "./js/Libs/laya.wxmini.js";
 import "./js/Libs/laya.webgl.js";
 import "./js/Libs/laya.html.js";
 import "./js/Libs/fairygui.min.js";
 import "./js/Libs/rc.js";
-import "./js/Main.js";
+import {Main} from "./js/Main.js";
+
+new Main();

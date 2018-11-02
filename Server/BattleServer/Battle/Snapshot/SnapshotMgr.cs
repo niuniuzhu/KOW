cc83b5baea2cc518f167332771e0ca6d88cb152a
Ron <niuniuzhu.cn@gmail.com>
2018-11-02 16:53:55 +0800

    修复快照管理器不能正确获取最新快照的bug

--- Server/BattleServer/Battle/Snapshot/SnapshotMgr.cs
+++ Server/BattleServer/Battle/Snapshot/SnapshotMgr.cs
@@ -1,4 +1,5 @@
-﻿using Google.Protobuf;
+﻿using System.Collections;
+using Google.Protobuf;
 using System.Collections.Generic;
 
 namespace BattleServer.Battle.Snapshot
@@ -11,7 +12,7 @@ namespace BattleServer.Battle.Snapshot
 		/// <summary>
 		/// 帧数到快照的映射表
 		/// </summary>
-		private readonly Dictionary<int, FrameSnapshot> _frameToSnapshot = new Dictionary<int, FrameSnapshot>();
+		private readonly SortedList<int, FrameSnapshot> _frameToSnapshot = new SortedList<int, FrameSnapshot>();
 
 		public void Clear()
 		{
@@ -21,10 +22,25 @@ namespace BattleServer.Battle.Snapshot
 		/// <summary>
 		/// 获取指定帧数下的快照实例
 		/// </summary>
-		public FrameSnapshot Get( int frame )
+		/// <param name="frame">指定帧数下的快速,-1表示最近的快照</param>
+		public FrameSnapshot Get( int frame = -1 )
 		{
+			if ( frame < 0 )
+			{
+				IList<FrameSnapshot> values = this._frameToSnapshot.Values;
+				int count = values.Count;
+				return count == 0 ? null : values[count - 1];
+			}
 			this._frameToSnapshot.TryGetValue( frame, out FrameSnapshot snapshot );
 			return snapshot;
+		}
+
+		/// <summary>
+		/// 保存快照
+		/// </summary>
+		public void Set( FrameSnapshot snapshot )
+		{
+			this._frameToSnapshot[snapshot.frame] = snapshot;
 		}
 
 		/// <summary>
@@ -37,7 +53,7 @@ namespace BattleServer.Battle.Snapshot
 				frame = frame,
 				data = data
 			};
-			this._frameToSnapshot[frame] = snapshot;
+			this.Set( snapshot );
 		}
 	}
 }

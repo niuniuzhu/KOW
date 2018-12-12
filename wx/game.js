4a5b128ab72ee82464f7ad0ed9bed06c57e87b98
Ron <niuniuzhu.cn@gmail.com>
2018-12-12 11:55:00 +0800

    fix file name not correct

--- wx/game.js
+++ wx/game.js
@@ -1,7 +1,6 @@
 import "./js/Libs/weapp-adapter.js";
 import "./js/Libs/laya.core.min.js";
 import "./js/Libs/laya.wxmini.min.js";
-import "./js/Libs/laya.webgl.min.js";
 import "./js/Libs/laya.html.min.js";
 import "./js/Libs/fairygui.min.js";
 import {Main} from "./js/Main.js";

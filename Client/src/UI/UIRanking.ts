00bfaedf43c8063f2ef4c6355563322b2e9b6d4a
Ron <niuniuzhu.cn@gmail.com>
2019-03-06 00:32:02 +0800

    完善排行榜

--- Client/src/UI/UIRanking.ts
+++ Client/src/UI/UIRanking.ts
@@ -7,12 +7,12 @@ export class UIRanking extends fairygui.Window {
 
 	constructor() {
 		super();
-
 		fairygui.UIPackage.addPackage("res/ui/ranking");
 	}
 
 	protected onInit(): void {
 		this.contentPane = fairygui.UIPackage.createObject("ranking", "Main").asCom;
+		this.modal = true;
 		this._list = this.contentPane.getChild("n13").asList;
 	}
 
@@ -22,7 +22,7 @@ export class UIRanking extends fairygui.Window {
 		const request = ProtoCreator.Q_GC2CS_QueryRanking();
 		request.sortType = Protos.GC2CS_QueryRanking.SortType.Rank;
 		request.from = 0;
-		request.count = 10;
+		request.count = 100;
 		Global.connector.SendToCS(Protos.GC2CS_QueryRanking, request, resp => {
 			this.closeModalWait();
 			const data = <Protos.CS2GC_QueryRankingRet>resp;
@@ -30,7 +30,7 @@ export class UIRanking extends fairygui.Window {
 			for (let i = 0; i < count; ++i) {
 				const rankingInfo = data.rankingInfos[i];
 				const item = this._list.addItemFromPool().asCom;
-				item.getChild("n29").asTextField.text = "" + i;
+				item.getChild("n29").asTextField.text = "" + (i + 1);
 				const loader = item.getChild("n19").asCom.getChild("loader").asLoader;
 				loader.url = rankingInfo.avatar;
 				item.getChild("n25").asTextField.text = rankingInfo.name;
--- wx/js/UI/UIRanking.js
+++ wx/js/UI/UIRanking.js
@@ -8,6 +8,7 @@ export class UIRanking extends fairygui.Window {
     }
     onInit() {
         this.contentPane = fairygui.UIPackage.createObject("ranking", "Main").asCom;
+        this.modal = true;
         this._list = this.contentPane.getChild("n13").asList;
     }
     onShown() {
@@ -16,7 +17,7 @@ export class UIRanking extends fairygui.Window {
         const request = ProtoCreator.Q_GC2CS_QueryRanking();
         request.sortType = Protos.GC2CS_QueryRanking.SortType.Rank;
         request.from = 0;
-        request.count = 10;
+        request.count = 100;
         Global.connector.SendToCS(Protos.GC2CS_QueryRanking, request, resp => {
             this.closeModalWait();
             const data = resp;
@@ -24,7 +25,7 @@ export class UIRanking extends fairygui.Window {
             for (let i = 0; i < count; ++i) {
                 const rankingInfo = data.rankingInfos[i];
                 const item = this._list.addItemFromPool().asCom;
-                item.getChild("n29").asTextField.text = "" + i;
+                item.getChild("n29").asTextField.text = "" + (i + 1);
                 const loader = item.getChild("n19").asCom.getChild("loader").asLoader;
                 loader.url = rankingInfo.avatar;
                 item.getChild("n25").asTextField.text = rankingInfo.name;
